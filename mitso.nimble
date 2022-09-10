5f055833c50b6061c4ae9ed80ab6a777389b296d
bit0r1n <me@bitor.in>
2022-09-11 00:49:33 +0300

    feat: made hybrid module/app

--- mitso.nimble
+++ mitso.nimble
@@ -5,6 +5,7 @@ author        = "bit0r1n"
 description   = "крутой бот митсо расписание арбуз парсинг"
 license       = "GPL-3.0-or-later"
 srcDir        = "src"
+installExt    = @["nim"]
 bin           = @["mitso"]
 
 
--- src/mitso.nim
+++ src/mitso.nim
@@ -1,37 +1,40 @@
-import sequtils, strutils, asyncdispatch, times
 import parser, typedefs, helpers
 
-proc main(): Future[void] {.async.} =
-  var parser = newSite()
+when not isMainModule:
+  export parser, typedefs, helpers
+else:
+  import sequtils, strutils, asyncdispatch
+  proc main(): Future[void] {.async.} =
+    var parser = newSite()
 
-  echo "Загрузка страницы"
-  discard await parser.loadPage()
+    echo "Загрузка страницы"
+    discard await parser.loadPage()
 
-  echo "Получение факультетов"
-  discard parser.getFaculties()
+    echo "Получение факультетов"
+    discard parser.getFaculties()
 
-  echo "Получение групп"
-  discard await parser.getGroups()
+    echo "Получение групп"
+    discard await parser.getGroups()
 
-  echo "Поиск группы \"2121\" второго курса"
-  let groups = parser.groups.filter do (x: Group) -> bool: x.display.contains("2121") and x.course == cSecond
+    echo "Поиск группы \"2121\" второго курса"
+    let groups = parser.groups.filter do (x: Group) -> bool: x.display.contains("2121") and x.course == cSecond
 
-  if groups.len == 0:
-    echo "Группа не найдена"
-  else:
-    echo "Поиск доступных недель расписания"
-    let weeks = await groups[0].getWeeks()
-    
-    if weeks.len == 0:
-      echo "Расписания нету"
+    if groups.len == 0:
+      echo "Группа не найдена"
     else:
-      echo "Получение расписания первой недели"
+      echo "Поиск доступных недель расписания"
+      let weeks = await groups[0].getWeeks()
+      
+      if weeks.len == 0:
+        echo "Расписания нету"
+      else:
+        echo "Получение расписания первой недели"
 
-      let scheldue = await groups[0].getScheldue(weeks[0])
+        let scheldue = await groups[0].getScheldue(weeks[0])
 
-      for day in scheldue:
-        echo day.displayDate & ", " & $day.day
-        for lesson in day.lessons:
-          echo "\t" & $lesson
+        for day in scheldue:
+          echo day.displayDate & ", " & $day.day
+          for lesson in day.lessons:
+            echo "\t" & $lesson
 
-waitFor main()
+  waitFor main()
