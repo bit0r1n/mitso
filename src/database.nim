08ad508638063f231a4a02c337ba61fe2f04ebe8
bit0r1n <me@bitor.in>
2022-09-15 00:28:41 +0300

    feat: bot functionality

---
+++ src/database.nim
@@ -0,0 +1,21 @@
+import strutils, redis, asyncdispatch
+type
+  DatabaseUserState* = enum
+    usAskingGroup
+    usChoosingGroup
+    usMainMenu
+
+proc state(id: int64): string = "user_state:" & $id
+
+proc stateExists*(r: Redis | AsyncRedis, user: int64): Future[bool] {.multisync.} =
+  result = await r.exists(user.state)
+
+proc setState*(r: Redis | AsyncRedis, user: int64, state: DatabaseUserState): Future[void] {.multisync.} =
+  await r.setK(user.state, $state)
+
+proc getState*(r: Redis | AsyncRedis, user: int64): Future[DatabaseUserState] {.multisync.} =
+  let res = await r.get(user.state)
+  if res == redisNil:
+    raise newException(CatchableError, "User state not found")
+  else:
+    result = parseEnum[DatabaseUserState](res)
--- src/mitso.nim
+++ src/mitso.nim
@@ -5,22 +5,44 @@ when not isMainModule:
 else:
   import asyncdispatch, os, strutils, options
   import telebot, redis
-  let
-    bot = newTeleBot(getEnv("BOT_TOKEN"))
-    redisClient = await openAsync(
-      getEnv("DB_HOST"),
-      parseInt(getEnv("DB_PORT")).Port
-    )
+  import database
 
-  if getEnv("DB_AUTH").len != 0:
-    if getEnv("DB_USER").len != 0:
-      await redisClient.auth(getEnv("DB_USER"), getEnv("DB_AUTH"))
-    else:
-      await redisClient.auth(getEnv("DB_AUTH"))
+  proc main() {.async.} =
+    let
+      bot = newTeleBot(getEnv("BOT_TOKEN"))
+      redisClient = await openAsync(
+        getEnv("DB_HOST"),
+        parseInt(getEnv("DB_PORT")).Port
+      )
 
-  proc updateHandler(b: Telebot, u: Update): Future[bool] {.gcsafe, async.} =
-    if u.message.isSome:
-      discard await b.sendMessage(u.message.get().chat.id, "test")
+    if getEnv("DB_AUTH").len != 0:
+      if getEnv("DB_USER").len != 0:
+        await redisClient.auth(getEnv("DB_USER"), getEnv("DB_AUTH"))
+      else:
+        await redisClient.auth(getEnv("DB_AUTH"))
 
-  bot.onUpdate(updateHandler)
-  bot.poll(timeout = 300) 
+    proc startCommand(b: Telebot, c: Command): Future[bool] {.async, gcsafe.} =
+      let uExists = await redisClient.stateExists(c.message.chat.id)
+      if not uExists:
+        await redisClient.setState(c.message.chat.id, usAskingGroup)
+        discard await b.sendMessage(c.message.chat.id, "🍉 Привет, я могу тебе показывать расписание!\nТолько мне сначала нужно указать группу 😫")
+        discard await b.sendMessage(c.message.chat.id, "🍳 Давай найдем твою группу. Напиши её номер.")
+      else:
+        #[ 
+          check for state and respond with required content
+            usAskingGroup - we still searching for group
+            usChoosingGroup - hey, choose group, or click cancel (set keyboard ["cancel"])
+            usMainMenu - here is your menu (set keyboard [[...scheldue], ["change group", ...idk]])
+        ]#
+        return true
+
+    proc updateHandler(b: Telebot, u: Update): Future[bool] {.async, gcsafe.} =
+      if u.message.isSome:
+        let state = await redisClient.getState(u.message.get().chat.id)
+        discard await b.sendMessage(u.message.get().chat.id, "test")
+
+    bot.onCommand("start", startCommand)
+    bot.onUpdate(updateHandler)
+    bot.poll(timeout = 300)
+
+  waitFor main()
